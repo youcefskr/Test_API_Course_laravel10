d5b1dc3c06becbc0b0109e350b0841cb3c527c90
sakerYoucef <youcefsaker09@gmail.com>
2024-07-07 23:53:36 +0100

    validation request response in postman

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Validator;
 use App\Models\posts;
 
 class PostsController extends Controller
@@ -12,7 +13,7 @@ class PostsController extends Controller
     use ApiResponseTrait;
     public function index()
     {
-        $posts = posts::get();
+        $posts = PostResource::collection(posts::get()) ;
 
         return $this->apiResponse($posts,'ok',200);
     }
@@ -21,8 +22,25 @@ class PostsController extends Controller
         $post = posts::find($id);
         if($post)
         {
-            return $this->apiResponse($post,'ok',200);
+            return $this->apiResponse(new PostResource($post),'ok',200);
         }
-        return $this->apiResponse(null,'The Post Not Found',401);
+        return $this->apiResponse(null,'The Post Not Found',404);
+    }
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|unique:posts|max:255',
+            'body' => 'required',
+        ]);
+        if ($validator->fails()) {
+            return $this->apiResponse(null,'The Post Not save',400);
+        }
+
+        $post =  posts::create($request->all());
+        if($post){
+            return $this->apiResponse(new PostResource($post),'save',201);
+        }
+
+ return $this->apiResponse(null,'The Post Not save',400);
     }
 }
